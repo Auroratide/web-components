4f3c5c1808770f33e8f5c190534f66ed56cc57be
Timothy Foster <tf.auroratide@gmail.com>
2022-09-03 09:38:56 -0500

    Shortcut for running docs

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -3,6 +3,9 @@ import { plugin as mdPlugin, Mode } from 'vite-plugin-markdown'
 
 /** @type {import('vite').UserConfig} */
 const config = {
+	server: {
+		port: 3000,
+	},
 	plugins: [ mdPlugin({
 		mode: Mode.HTML,
 	}), sveltekit() ]
