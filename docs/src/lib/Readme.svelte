6d0a6d0be4b916060eb3385ecae3676b2119ec46
Timothy Foster <tf.auroratide@gmail.com>
2024-02-11 08:37:29 -0600

    Pre-commit hook for linting

--- docs/src/lib/Readme.svelte
+++ docs/src/lib/Readme.svelte
@@ -6,7 +6,7 @@
 	}[] = []
 
 	$: toRender = assets.reduce((html, asset) => {
-		return html.replace(new RegExp(asset.replace, 'g'), asset.with)
+		return html.replace(new RegExp(asset.replace, "g"), asset.with)
 	}, html)
 </script>
 
